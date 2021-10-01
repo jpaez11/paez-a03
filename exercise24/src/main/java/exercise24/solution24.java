0d1d234a14f52a7e5961feb27b3db154330d99bc
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-01 17:06:52 -0400

    solution for ex24

---
+++ exercise24/src/main/java/exercise24/solution24.java
@@ -0,0 +1,19 @@
+package exercise24;
+
+
+public class solution24 {
+
+           /*
+    Pseudocode:
+    making a program that will use arrays to tell if words are anagrams
+    or not, by using strings!
+    the process for this will be:
+    prompt user to give the two strings which will be scanned,
+    those two stings will be checked by another class (just to see if they are anagrams)
+    a comparison will take place in it, for two different outputs
+    one output will be to see if the strings are anagrams
+    the other input will be to say that they are not anagrams
+        */
+
+
+}
