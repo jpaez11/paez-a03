fc5c0372c8ec0f2f3d04c48e36c284befa74d574
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-02 03:48:47 -0400

    ex28

---
+++ exercise28/src/main/java/exercise28/solution28.java
@@ -0,0 +1,14 @@
+package exercise28;
+
+public class solution28 {
+    /*
+    Pseudocode:
+    5 scanned inputs will be read from the user
+    those 5 inputs will be added up to a total number
+    flowchart:
+    start > the total = 0 > set an int variable = 0 > when that int is <5 it will prompt
+    user to enter a number > when a number is inputted it will get the next input >
+    total = total + number > variable = variable + 1 (until 5, if not it will cycle the prompt again)
+    > demonstrates total output > end
+     */
+}
