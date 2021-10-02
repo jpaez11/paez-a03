e816b11a45f06d6a76b9ca91f715b9eee9fefc87
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-02 03:35:53 -0400

    ex30

--- exercise30/src/main/java/exercise30/solution30.java
+++ exercise30/src/main/java/exercise30/solution30.java
@@ -1,11 +1,19 @@
 package exercise30;
-import java.util.*;
 
+/*
+  Pseudocode:
+  using a nested for loop, this program will make a multiplication
+  table. from 1-12 specifically.
+   */
 public class solution30 {
-
-    /*
-    Pseudocode:
-    using a nested for loop, this program will make a multiplication
-    table. from 1-12 specifically.
-     */
+    public static void main(String[] args) {
+        //making a for loop inside a for loop
+        //feels weird not scanning user input
+        for (int a = 1; a <= 12; a++) {
+            for (int b = 1; b <= 12; b++) {
+                System.out.print((a * b) + "\t"); //and this is how it becomes a table
+            }
+            System.out.println();//output displayed
+        }
+    }
 }
