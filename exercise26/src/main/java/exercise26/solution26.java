9fcdb1240fac5757dcd8d57658e28294a04fc932
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-01 23:17:20 -0400

    solutions for ex26

---
+++ exercise26/src/main/java/exercise26/solution26.java
@@ -0,0 +1,17 @@
+package exercise26;
+
+/*
+Pseudocode:
+using the formula n = -(1/30) * log(1 + b/p * (1 - (1 + i)^30)) / log(1 + i)
+the program will calculate the amount of months needed to pay of credit card debt
+there will be a class named PaymentCalculator
+with a public method called calculateMonthsUntilPaidOff, which returns the number of months.
+the process:
+first and foremost using Math, the formula will be calc'd
+then a scanner for user input
+and then the months will be calc'd and shown
+ */
+
+public class solution26 {
+
+}
