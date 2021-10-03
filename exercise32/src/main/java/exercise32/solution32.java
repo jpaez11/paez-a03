2a8b68f51f0be1491d3dd9a0648cb14847479c06
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-03 02:39:48 -0400

    ex32

---
+++ exercise32/src/main/java/exercise32/solution32.java
@@ -0,0 +1,17 @@
+package exercise32;
+
+public class solution32 {
+    /*
+    Pseudocode:
+    let's guess some numbers, but not allow the user to input non
+    numerical values. like never during the program, except when they
+    want to not guess anymore numbers. this game will consist of 3 levels,
+    the 1st one would only feature 1-10, the 2nd one would be btwn 1-100,
+    and the last one would be btwn 1-1000.
+    the process:
+    scanning user input
+    making a while loop for the game to run
+    a break clause to end the loop
+    randomizing numbers for all difficulties
+     */
+}
