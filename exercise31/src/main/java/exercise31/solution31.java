2253b68c9a6f54d4b15d91b743ca21932a5c6da5
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-03 00:45:17 -0400

    ex31

---
+++ exercise31/src/main/java/exercise31/solution31.java
@@ -0,0 +1,13 @@
+package exercise31;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class solution31 {
+    /*
+    Pseudocode:
+    using the karvonean heart rate formula: TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
+     the program will also make sure that only numerical numbers are entered, results will be neatly showed in
+     a table.
+     */
+}
