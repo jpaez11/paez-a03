8df637c564fdf9d3823d74d6f950c7836d086aec
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-03 15:51:47 -0400

    ex33

--- exercise33/src/main/java/exercise33/solution33.java
+++ exercise33/src/main/java/exercise33/solution33.java
@@ -1,9 +1,33 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jorge Paez
+ */
 package exercise33;
+import java.util.*;
 
+/*
+  Pseudocode:
+      this program will make a magic 8ball that will give randomly
+      stored answers
+   */
 public class solution33 {
-    /*
-    Pseudocode:
-        this program will make a magic 8ball that will give randomly
-        stored answers
-     */
+  public static void main (String [] args){
+      //responses to be stored and given randomly
+
+      String[] responses = {"No", "Yes", "Ask again later", "Maybe"};
+      Random rng = new Random(); //it's random innit?
+      Scanner sca = new Scanner(System.in);
+
+      System.out.println("What is your question?");
+      String ask = sca.nextLine(); //here's the question
+
+      //here's how the options will be selection
+      int randomResponses = rng.nextInt(responses.length);
+      String result = responses[randomResponses];
+
+      //printing the responses
+      System.out.println();
+      System.out.println(result);
+      System.out.println();
+  }
 }
