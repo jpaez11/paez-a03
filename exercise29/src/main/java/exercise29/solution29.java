62b1fea3731d80b03faf16052fa8066165755e29
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-02 21:35:03 -0400

    ex29

---
+++ exercise29/src/main/java/exercise29/solution29.java
@@ -0,0 +1,17 @@
+package exercise29;
+
+public class solution29 {
+    /*
+    Psuedocode:
+    using the formula years = 72 / r, the program
+    will be a quick calculator that will prompt user on a
+    return in investment. it will also not allow user to enter
+    wrong inputs.
+    the process:
+    scanning the user inputs
+    making a loop to trap any wrong inputs
+    and a try and catch method for any exception
+     */
+
+}
+
