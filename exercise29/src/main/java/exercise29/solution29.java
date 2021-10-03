f73cae5bfcf46f2c46c13814d91edaa771198e95
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-02 21:54:34 -0400

    ex29

--- exercise29/src/main/java/exercise29/solution29.java
+++ exercise29/src/main/java/exercise29/solution29.java
@@ -1,8 +1,8 @@
 package exercise29;
+import java.util.*;
 
-public class solution29 {
     /*
-    Psuedocode:
+    Pseudocode:
     using the formula years = 72 / r, the program
     will be a quick calculator that will prompt user on a
     return in investment. it will also not allow user to enter
@@ -12,6 +12,32 @@ public class solution29 {
     making a loop to trap any wrong inputs
     and a try and catch method for any exception
      */
+
+public class solution29 {
+    public static void main(String[] args){
+        Scanner sca = new Scanner(System.in); //hello user!
+        boolean wrongInput = false; //this is to have the loop condition for wrong inputs
+
+        //loop start
+        while(true){
+            try {
+                //prompt for the return
+            System.out.print("What's the rate of return?");
+            int rate= Integer.parseInt(sca.next()); //here's the interest rate
+            System.out.println("It'll take this many" + (72/rate) + "years to double your first investment. ");
+            wrongInput = true; //if the exception don't happen
+            }
+            catch (NumberFormatException exception){ //that input is wrong!!
+                System.out.println("Sorry. Not a valid input!");
+            }
+            catch(ArithmeticException exception){ //here's division, but wrong and caught by exception
+                System.out.println("Sorry. Not a valid input!");
+            }
+            if(wrongInput){ //if everything goes according to plan
+                break; //and this kills the loop
+            }
+        }
+    }
 
 }
 
