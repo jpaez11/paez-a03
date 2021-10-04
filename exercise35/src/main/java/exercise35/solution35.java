c5ec880157a250eaeeea98988e03bc9ec2110604
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-03 21:11:19 -0400

    ex35

--- exercise35/src/main/java/exercise35/solution35.java
+++ exercise35/src/main/java/exercise35/solution35.java
@@ -5,6 +5,28 @@ public class solution35 {
     /*
     Pseudocode:
     making an arrayList, using the random function, as well as scanning user input
-    a winner will be selected at random
+    a winner will be selected at random by using a loop
      */
+    public static void main(String [] args) {
+        Scanner scanner = new Scanner(System.in);
+        String winner = null;
+        ArrayList<String> listofNames = new ArrayList<String>();//storing the names
+
+        //loop to pick out the names
+        do {
+            System.out.println("Enter a name: ");
+            winner = scanner.nextLine(); //grabbing the name
+
+            //adding the winner to the arrayList, only if empty/not empty
+            if (winner.isBlank() && !winner.isEmpty())
+                listofNames.add(winner);
+        } while (!winner.isBlank() && !winner.isEmpty());
+
+        //rng to grab winner
+        Random rng = new Random();
+        int rngIndex = rng.nextInt(listofNames.size());
+
+        //and the winner is....
+        System.out.println("The winner is..... " + listofNames.get(rngIndex));
+    }
 }
