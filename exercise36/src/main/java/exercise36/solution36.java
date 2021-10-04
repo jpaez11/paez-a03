3721e18347a0bc81b701673a2a91c5527857fd83
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-03 21:17:53 -0400

    ex36

---
+++ exercise36/src/main/java/exercise36/solution36.java
@@ -0,0 +1,10 @@
+package exercise36;
+import java.util.*;
+public class solution36 {
+    /*
+    Pseudocode:
+    using functions called average, max, min, and std (in a list) will return
+    results of statistics. loops and arrays will be used for the mathematical
+    equations to function. program will also ignore nonvalid inputs.
+     */
+}
