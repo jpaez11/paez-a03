f77b44fa040a70e612ba888624d7588fa97bebcc
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-03 22:28:12 -0400

    ex39

---
+++ exercise39/src/main/java/exercise39/solution39.java
@@ -0,0 +1,9 @@
+package exercise39;
+import java.util.*;
+public class solution39 {
+    /*
+    Pseudocode:
+    using a map, a data set will be sorted by last name
+    and will be printed in a table.
+     */
+}
