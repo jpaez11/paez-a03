92b8ed3f10f686b47d326fdd269962204696f0d0
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-03 22:59:55 -0400

    ex40

---
+++ exercise40/src/main/java/exercise40/solution40.java
@@ -0,0 +1,10 @@
+package exercise40;
+import java.util.*;
+public class solution40 {
+
+    /*
+    Pseudocode
+    this program will filter out results to find what is needed from a given data set
+    this will be done by using a search string, it will be stored using maps
+     */
+}
