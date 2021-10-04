5121ad749523481409e53317fdbf34988104eaaf
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-03 20:50:06 -0400

    ex35

---
+++ exercise35/src/main/java/exercise35/solution35.java
@@ -0,0 +1,10 @@
+package exercise35;
+import java.util.*;
+
+public class solution35 {
+    /*
+    Pseudocode:
+    making an arrayList, using the random function, as well as scanning user input
+    a winner will be selected at random
+     */
+}
