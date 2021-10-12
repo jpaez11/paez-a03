77db9433f3ca255ae91e43a53b990220ec973ae6
jpaez11 <90153766+jpaez11@users.noreply.github.com>
2021-10-12 19:51:46 -0400

    Update solution24.java

--- exercise24/src/main/java/exercise24/solution24.java
+++ exercise24/src/main/java/exercise24/solution24.java
@@ -1,9 +1,11 @@
+
 package exercise24;
 
 
 public class solution24 {
 
-           /*
+          /*
+
     Pseudocode:
     making a program that will use arrays to tell if words are anagrams
     or not, by using strings!
@@ -15,5 +17,25 @@ public class solution24 {
     the other input will be to say that they are not anagrams
         */
 
+public class solution24 {
 
-}
+
+        public static void main(String[] args) { //main class to check anagrams
+            Scanner sc= new Scanner(System.in); //scanning...
+            //the prompt
+            System.out.println("Enter two strings and let's see if they're anagrams: \n");
+
+            //user please input the strings
+            System.out.print("Enter the first string: \n");
+            String str1=sc.nextLine();
+            System.out.print("Enter the second string: \n");
+            String str2=sc.nextLine();
+
+            //let's check those anagrams
+            if(forStrings.ForStrings.isAnagram(str1,str2))
+                System.out.println(str1 + " and " + str2 + " are anagrams");
+            else
+                System.out.println(str2 + " and " + str2 + " are not anagrams");
+
+        }
+    }
